00b6485cb618c711db0a1d40baf3586c63886bd9
Amal Jose <amal@dexlock.com>
2019-06-10 14:43:43 +0530

    working list implemented

---
+++ README.md
@@ -0,0 +1,2 @@
+node server.js (server)
+serve (client)
--- server.js
+++ server.js
@@ -9,13 +9,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 const render = (req, res) => {
     //console.log("req",req)
     console.log("req.params.number",req.params.number)
-    res.send('<h1>number is '+req.params.number+'</h1>');
+    let content=""
+    for(i=0;i<=req.params.number;i++){
+        content=content+'<h1>number is '+i+'</h1>'
+    }
+    res.send({"list":content});
 };
 
+const data = (req, res) => {
+    //console.log("req",req)
+    console.log("req.params.number",req.params.number)
+    res.send({"data":["a","b"]});
+}; 
+
 app.get('/html/:number', render);
+app.get('/data', data);
 
 app.listen(4000, () => console.log(' Server Now Running On localhost:4000'));
